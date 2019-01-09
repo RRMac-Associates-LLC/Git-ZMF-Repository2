b239129b6cc24eabeef690196ab33c2f26f77536
Robin Macfarlane <robin.macfarlane@rrmac.com>
2019-01-08 19:06:41 -0500

    Create rrmbs196.cbl
    add new source

---
+++ MortgageApplication/cobol/rrmbs196.cbl
@@ -0,0 +1,24 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. RRMBS196.
+      *
+      * Generated COBOL program
+      * by RRMAC Associates LLC on 2 Sep 2016 at 11:56:13
+      *
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      * NO-DB2
+      * NO-DB2
+           COPY
+            RRMBC196.
+
+       PROCEDURE DIVISION.
+      * NO-DB2
+      * NO-DB2
+      * NO-DB2
+           MOVE INCL-11-A TO INCL-21-A.
+      * NO-CICS
+      * NO-CICS
+      * NO-CICS
+           GOBACK.
+      *End of program RRMBS196.
